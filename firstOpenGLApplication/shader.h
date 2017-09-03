c6f08742d7fb53d6ef381b5ea8a03ed4d8da531b
Tucker Walker <tuckerdwalker@gmail.com>
2017-09-02 21:09:12 -0700

    added shader class

---
+++ firstOpenGLApplication/shader.h
@@ -0,0 +1,44 @@
+//************************************************************************************************************************
+//
+//	LearnOpenGL - shader.h
+//
+//	Name:			Tucker Dane Walker
+//	Date:			August 2017
+//	Description:	Specifications for an OpenGL shader class to make using shaders more streamlined
+//					Mostly taken from learnopengl.com
+//
+//***********************************************************************************************************************/
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+	// the program ID
+	//---------------------------------
+	unsigned int ID;
+
+	// constructor reads and builds the shader
+	//---------------------------------
+	Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+
+	// use/activate the shader
+	//---------------------------------
+	void use();
+
+	// utility uniform functions
+	//---------------------------------
+	void setBool(const std::string &name, bool value) const;
+	void setInt(const std::string &name, int value) const;
+	void setFloat(const std::string &name, float value) const;
+};
+
+#endif
